addc9b67a992d7fa07ab5b759031bdccbdd0e0dd
Kevin Ndung'u <kevgathuku@gmail.com>
2019-11-14 12:53:39 +0300

    Add movie <> reviews association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_092732) do
+ActiveRecord::Schema.define(version: 2019_11_14_095113) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_092732) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
