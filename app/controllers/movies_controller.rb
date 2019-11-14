279a728a76d11b5792d28dba5f08aef9dfe43032
Kevin Ndung'u <kevgathuku@gmail.com>
2019-11-14 10:56:38 +0300

    Enable attaching a poster to a movie via Active Storage
    - Add a poster field, and display it in the movie page if available

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -71,6 +71,6 @@ class MoviesController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def movie_params
-    params.require(:movie).permit(:title, :description, :runtime, :director, :rating)
+    params.require(:movie).permit(:title, :description, :runtime, :director, :rating, :poster)
   end
 end
